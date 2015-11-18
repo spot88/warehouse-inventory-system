9a72f871dde2115a6a30fc7cb5de179bb33621a9
antring <emauring@hotmail.com>
2015-11-18 15:29:45 +0100

    Fiksa #18

--- monthly_sales.php
+++ monthly_sales.php
@@ -1,14 +1,15 @@
 <?php
   $page_title = 'Monthly Sales';
   require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-   page_require_level(3);
-?>
-<?php
+
+// Checking userlevel
+   page_require_level(1);
+
  $year = date('Y');
  $sales = monthlySales($year);
+
+include_once('layouts/header.php');
 ?>
-<?php include_once('layouts/header.php'); ?>
 <div class="row">
   <div class="col-md-6">
     <?php echo display_msg($msg); ?>
