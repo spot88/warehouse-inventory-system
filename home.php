16566a010fa2d2aeb6f244d43350e010c826be29
antring <emauring@hotmail.com>
2015-11-22 21:40:54 +0100

    lagerfix på home.php

--- home.php
+++ home.php
@@ -12,7 +12,8 @@ $c_sale = count_by_id('sales');
 $c_user = count_by_id('users');
 $products_sold = find_higest_saleing_product('10');
 $recent_products = find_recent_product_added('5');
-$recent_sales = find_recent_sale_added('5')
+$recent_sales = find_recent_sale_added('5');
+$storageStatus = storage_status();
 ?>
 <?php include_once('layouts/header.php'); ?>
 <div class="row">
