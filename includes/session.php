89d6fd0a6a1954db8ed519d277cf39ca51089ad5
antring <emauring@hotmail.com>
2015-11-23 19:14:40 +0100

    Formatering

--- includes/session.php
+++ includes/session.php
@@ -1,56 +1,65 @@
 <?php
- session_start();
+session_start();
 
-class Session {
+class Session
+{
 
- public $msg;
- private $user_is_logged_in = false;
+    public $msg;
+    private $user_is_logged_in = false;
 
- function __construct(){
-   $this->flash_msg();
-   $this->userLoginSetup();
- }
-
-  public function isUserLoggedIn(){
-    return $this->user_is_logged_in;
-  }
-  public function login($user_id){
-    $_SESSION['user_id'] = $user_id;
-  }
-  private function userLoginSetup()
-  {
-    if(isset($_SESSION['user_id']))
+    function __construct()
     {
-      $this->user_is_logged_in = true;
-    } else {
-      $this->user_is_logged_in = false;
+        $this->flash_msg();
+        $this->userLoginSetup();
     }
 
-  }
-  public function logout(){
-    unset($_SESSION['user_id']);
-  }
+    private function flash_msg()
+    {
 
-  public function msg($type ='', $msg =''){
-    if(!empty($msg)){
-       if(strlen(trim($type)) == 1){
-         $type = str_replace( array('d', 'i', 'w','s'), array('danger', 'info', 'warning','success'), $type );
-       }
-       $_SESSION['msg'][$type] = $msg;
-    } else {
-      return $this->msg;
+        if (isset($_SESSION['msg'])) {
+            $this->msg = $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        } else {
+            $this->msg;
+        }
     }
-  }
 
-  private function flash_msg(){
+    private function userLoginSetup()
+    {
+        if (isset($_SESSION['user_id'])) {
+            $this->user_is_logged_in = true;
+        } else {
+            $this->user_is_logged_in = false;
+        }
 
-    if(isset($_SESSION['msg'])) {
-      $this->msg = $_SESSION['msg'];
-      unset($_SESSION['msg']);
-    } else {
-      $this->msg;
     }
-  }
+
+    public function isUserLoggedIn()
+    {
+        return $this->user_is_logged_in;
+    }
+
+    public function login($user_id)
+    {
+        $_SESSION['user_id'] = $user_id;
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+    }
+
+    public function msg($type = '', $msg = '')
+    {
+        if (!empty($msg)) {
+            if (strlen(trim($type)) == 1) {
+                $type = str_replace(array('d', 'i', 'w', 's'), array('danger', 'info', 'warning', 'success'), $type);
+            }
+            $_SESSION['msg'][$type] = $msg;
+        } else {
+            return $this->msg;
+        }
+    }
 }
 
 $session = new Session();
