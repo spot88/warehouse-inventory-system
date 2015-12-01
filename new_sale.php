21981919521e36ec6bdd80d7424dd99d116e1a63
antring <emauring@hotmail.com>
2015-12-01 01:37:31 +0100

    update

--- new_sale.php
+++ new_sale.php
@@ -23,10 +23,14 @@ if ($isAdmin) {
     $products = get_products_user();
 }
 
+if (isset($_POST['custnr'])) {
+    echo var_dump($_POST['sale']);
+    echo "banana";
+}
 
 ?>
-<script type="text/javascript" src="add_prod.js"></script>
 <script type="text/javascript" src="includes/jquery.js"></script>
+<script type="text/javascript" src="add_prod.js"></script>
 <div class="col-md-12">
     <div class="panel panel-default">
         <div class="panel-heading clearfix">
@@ -36,35 +40,57 @@ if ($isAdmin) {
             </strong>
         </div>
         <div class="panel-body">
-            <form method="post" action="">
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-2">
-                            <div class="input-group">
+            <div id="productButton">
+                <?php foreach ($products as $prod): ?>
+                    <button name="<?php echo($prod['id']); ?>" class="btn btn-danger"><?php echo $prod['name']; ?></button>
+                <?php endforeach; ?>
+            </div>
+            <form>
+            <div class="form-group" style="margin-top: 15px">
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="input-group">
                                 <span class="input-group-addon">
                                       <i class="glyphicon glyphicon-info-sign"></i>
                                  </span>
-                                <input class="form-control" type="number" min="0" max="10000000" size="8" name="custnr" placeholder="Kundenummer" autocomplete="off">
-                            </div>
+                            <input class="form-control" type="number" min="0" max="10000000" size="8" name="custnr" placeholder="Kundenummer" autocomplete="on" required>
                         </div>
+                    </div>
 
-                        <div class="col-md-2">
-                            <div class="input-group">
+                    <div class="col-md-2">
+                        <div class="input-group">
                                 <span class="input-group-addon">
                                       <i class="glyphicon glyphicon-info-sign"></i>
                                  </span>
-                                <input class="form-control" type="text" name="comment" placeholder="Kommentar">
-                            </div>
+                            <input type="date" class="form-control datePicker" name="date" data-date data-date-format="yyyy-mm-dd" required>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2">
+                        <div class="input-group">
+                                <span class="input-group-addon">
+                                      <i class="glyphicon glyphicon-info-sign"></i>
+                                 </span>
+                            <textarea rows="1" class="form-control" type="text" name="comment" placeholder="Kommentar" required></textarea>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <form method="post" action="ajax.php">
-                        <?php foreach ($products as $prod): ?>
-                            <button id="#sug-form" type="submit" name="<?php echo($prod['id']); ?>" class="btn btn-danger"><?php echo $prod['name']; ?></button>
-                        <?php endforeach; ?>
-                    </form>
-                </div>
+            </div>
+            <table class="table table-bordered">
+                <thead>
+                <th> Produkt</th>
+                <th> Pris</th>
+                <th> Antall</th>
+                <th> Total</th>
+                <th></th>
+                </thead>
+
+                <tbody id="result">
+
+                </tbody>
+                <button type="submit" name="sale" class="btn btn-primary">Add sale</button>
+                </form>
+            </table>
         </div>
     </div>
 </div>
